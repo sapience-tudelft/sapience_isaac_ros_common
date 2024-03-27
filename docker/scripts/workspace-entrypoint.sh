e498f1c62fe552a2d306acea26f9c1e23eef669f
maximecapelle <max.luc.cap@gmail.com>
2024-03-27 13:43:29 +0100

    attempts

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -15,7 +15,18 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 sudo apt-get update
 rosdep update
 
+# MAVROS
+rosdep install --from-paths ${ROS_WORKSPACE} --ignore-src --rosdistro=${ROSDISTRO}
+sudo apt install -y python3-vcstool python3-rosinstall-generator python3-osrf-pycommon
+rosinstall_generator --format repos mavlink | tee /tmp/mavlink.repos
+rosinstall_generator --format repos --upstream mavros | tee -a /tmp/mavros.repos
+vcs import src < /tmp/mavlink.repos
+vcs import src < /tmp/mavros.repos
+rosdep install --from-paths src --ignore-src -y
+sudo ./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
+colcon build --packages-select mavros mavlink
 
+# Gazebo Garden
 export GZ_VERSION=garden
 rosdep install -r --from-paths src -i -y --rosdistro humble
 colcon build
