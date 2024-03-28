ed6f0bb4bc1ada640427126c02ecbac7394d667b
maximecapelle <max.luc.cap@gmail.com>
2024-03-28 21:55:22 +0100

    Fully working and cleaned code

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -15,28 +15,13 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 sudo apt-get update
 rosdep update
 
-# # Gazebo Garden
-# export GZ_VERSION=garden
-# rosdep install -r --from-paths src -i -y --rosdistro humble
+# Gazebo Garden
+export GZ_VERSION=garden
+rosdep install -r --from-paths src -i -y --rosdistro humble
 
-# # MAVROS
-# cd /workspaces/sapience_nvidia_devel
-# sudo apt install -y python3-vcstool python3-rosinstall-generator python3-osrf-pycommon
-# rosinstall_generator --format repos mavlink | tee /tmp/mavlink.repos
-# rosinstall_generator --format repos --upstream mavros | tee -a /tmp/mavros.repos
-# vcs import src < /tmp/mavlink.repos
-# vcs import src < /tmp/mavros.repos
-# rosdep update && rosdep install -y -i --from-paths src
-# rosdep install --from-paths ${ROS_WORKSPACE} --ignore-src --rosdistro=${ROSDISTRO}
-# sudo ./src/mavros/mavros/scripts/install_geographiclib_datasets.sh
-# colcon build --packages-select mavros mavlink
-
-
-# colcon build
-# . install/setup.bash
-
-# pip install pexpect pymavlink
-
+# Build packages
+colcon build
+. install/setup.bash
 
 # Restart udev daemon
 sudo service udev restart
